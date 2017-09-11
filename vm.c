bc37b9ff7aeb2eecdb219ffd74146f3c2fdbdca0
rmit-s3662617-ben-osullivan <s3662617@student.rmit.edu.au>
2017-09-11 23:54:02 +1000

    First commit

---
+++ vm.c
@@ -0,0 +1,8 @@
+#include "vm.h"
+
+int main(int argc, char ** argv)
+{
+    printf("Goodbye. \n\n");
+
+    return EXIT_SUCCESS;
+}
---
+++ vm_stock.h
@@ -0,0 +1,14 @@
+#ifndef VM_STOCK_H
+#define VM_STOCK_H
+
+#include "vm_coin.h"
+
+/**
+ * The default stock level that all new stock should start with and the value
+ * to use when restting stock.
+ **/
+#define DEFAULT_STOCK_LEVEL 20
+
+#define STOCK_DELIM "|"
+
+#endif
